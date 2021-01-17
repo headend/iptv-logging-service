5593e8488c1449faca859a1a14e41be3c65fb204
thuonghh <thuonghh@vng.com.vn>
2021-01-17 23:06:41 +0700

    db default 0 for statsu

--- model/model.go
+++ model/model.go
@@ -10,16 +10,16 @@ type MonitorLogs struct {
 	Id  int64 `gorm:"column:id;AUTO_INCREMENT;primary_key"`
 	AgentID int64 `gorm:"column:agent_id;default:null"`
 	ProfileID int64 `gorm:"column:profile_id;default:null"`
-	MonitorID  int64 `gorm:"column:monitor_id ;default:null"`
-	ChannelID  int64 `gorm:"column:channel_id ;default:null"`
+	MonitorID  int64 `gorm:"column:monitor_id;default:null"`
+	ChannelID  int64 `gorm:"column:channel_id;default:null"`
 	ChannelName string `gorm:"column:channel_name;default:null"`
 	MulticastIP string `gorm:"column:multicast_ip;default:null"`
 	BeforeStatus int64 `gorm:"column:before_status;default:null"`
-	BeforeSignalStatus  bool  `gorm:"column:before_signal_status;default:true"`
+	BeforeSignalStatus  bool  `gorm:"column:before_signal_status;default:false"`
 	BeforeVideoStatus bool  `gorm:"column:before_video_status;default:false"`
 	BeforeAudioStatus bool  `gorm:"column:before_audio_status;default:false"`
 	AfterStatus int64 `gorm:"column:after_status;default:null"`
-	AfterSignalStatus  bool  `gorm:"column:after_signal_status;default:true"`
+	AfterSignalStatus  bool  `gorm:"column:after_signal_status;default:false"`
 	AfterVideoStatus bool  `gorm:"column:after_video_status;default:false"`
 	AfterAudioStatus bool  `gorm:"column:after_audio_status;default:false"`
 	Description string `gorm:"column:desc;default:null"`
