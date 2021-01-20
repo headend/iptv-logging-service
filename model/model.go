450cd0d84e746ba3135b9578562bd1312a4cbf99
thuonghh <thuonghh@vng.com.vn>
2021-01-20 22:50:12 +0700

    set index

--- model/model.go
+++ model/model.go
@@ -8,12 +8,12 @@ func (MonitorLogs) TableName() string {
 
 type MonitorLogs struct {
 	Id  int64 `gorm:"column:id;AUTO_INCREMENT;primary_key"`
-	AgentID int64 `gorm:"column:agent_id;default:null"`
-	ProfileID int64 `gorm:"column:profile_id;default:null"`
-	MonitorID  int64 `gorm:"column:monitor_id;default:null"`
-	ChannelID  int64 `gorm:"column:channel_id;default:null"`
-	ChannelName string `gorm:"column:channel_name;default:null"`
-	MulticastIP string `gorm:"column:multicast_ip;default:null"`
+	AgentID int64 `gorm:"index:btree;column:agent_id;default:null"`
+	ProfileID int64 `gorm:"index:btree;column:profile_id;default:null"`
+	MonitorID  int64 `gorm:"index:btree;column:monitor_id;default:null"`
+	ChannelID  int64 `gorm:"index:btree;column:channel_id;default:null"`
+	ChannelName string `gorm:"index;column:channel_name;default:null"`
+	MulticastIP string `gorm:"index;column:multicast_ip;default:null"`
 	BeforeStatus int64 `gorm:"column:before_status;default:null"`
 	BeforeSignalStatus  bool  `gorm:"column:before_signal_status;default:false"`
 	BeforeVideoStatus bool  `gorm:"column:before_video_status;default:false"`
